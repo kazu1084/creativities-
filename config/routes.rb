dfad20c678d6efb64909fd3d4c3ebca5bb999417
kazuhikokayumi <56419952+kazu1084@users.noreply.github.com>
2024-11-08 07:14:54 +0000

    comment

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'homes/about' => 'homes#about',as: 'about'
   resources :posts,only: [:index, :show, :new, :edit, :create, :destroy, :update] do
-      resources :comments,only: [:create]
+      resources :comments,only: [:create,:destroy]
   end
   resources :clients,only: [:show, :edit, :update] do
     get 'mypage' => 'clients#mypage'
