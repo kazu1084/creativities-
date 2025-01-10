181bab38151aea430461c7384066a18f7cf256e7
kazuhikokayumi <56419952+kazu1084@users.noreply.github.com>
2025-01-10 02:40:40 +0000

    profileimage-error

--- app/models/client.rb
+++ app/models/client.rb
@@ -36,7 +36,7 @@ class Client < ApplicationRecord
 
   def get_profile_image(width,height)
     unless profile_image.attached?
-      file_path=Rails.root.join("app.assets/images/no_image")
+      file_path=Rails.root.join("app/assets/images/no_image.jpg")
       profile_image.attach(io:File.open(file_path),filename:"default-image.jpg",content_type:"image/jpeg")
     end
     profile_image.variant(resize_to_limit:[width,height]).processed
@@ -44,7 +44,7 @@ class Client < ApplicationRecord
 
   def icon_image
     unless profile_image.attached?
-      file_path=Rails.root.join("app.assets/images/no_image")
+      file_path=Rails.root.join("app/assets/images/no_image.jpg")
       profile_image.attach(io:File.open(file_path),filename:"default-image.jpg",content_type:"image/jpeg")
     end
     profile_image.variant(resize_to_fill: [100,100, { gravity: "Center" }]).processed
