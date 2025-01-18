6d3fdac9fae99845a7a9f392fb3cd52e64bc01b6
kazuhikokayumi <56419952+kazu1084@users.noreply.github.com>
2025-01-18 02:25:49 +0000

    notifications-controller-fixies

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,9 +1,9 @@
 class NotificationsController < ApplicationController
   def index
     @current_user = current_client || current_contractor
-    @notifications = @current_user.passive_notifications.page(params[:page]).per(20)
-    @notifications.where(checked: false).each do |notification|
-      notification.update_attributes(checked: true)
+    @notifications = @current_user.passive_notifications.where.not(visitor_id: @current_user.id).page(params[:page]).per(20)
+    @notifications.where(read: false).each do |notification|
+      notification.update_attributes(read: true)
     end
   end
 end
